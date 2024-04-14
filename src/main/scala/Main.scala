ac5d0a9314bbcc250b16af640d3f89d981bbe50a
Christopher Spears <cspears2002@gmail.com>
2024-04-14 14:28:54 -0700

    Reverse the lines in a file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,23 @@
+import java.io.PrintWriter
+import scala.io.Source
+import scala.compiletime.ops.double
+
 @main def hello(): Unit =
   println("Hello world!")
   println(msg)
 
+  var path = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex1.txt"
+  var outputPath = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex1_answer.txt"
+  reverseLines(path, outputPath)
+
+
 def msg = "I was compiled by Scala 3. :)"
+
+
+def reverseLines(path: String, outputPath: String ): Unit =
+  var source = Source.fromFile(path)
+  var lineArray = source.getLines.toArray
+  val out = PrintWriter(outputPath)
+  lineArray.reverse.foreach(out.println)
+  out.close()
+
