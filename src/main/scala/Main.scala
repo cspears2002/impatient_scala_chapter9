6303750ebf0cc23abba825c0a53e0580711dd4fe
Christopher Spears <cspears2002@gmail.com>
2024-04-14 15:02:33 -0700

    Four spaces for tabs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,21 +3,38 @@ import scala.io.Source
 import scala.compiletime.ops.double
 
 @main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+    println("Hello world!")
+    println(msg)
 
-  var path = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex1.txt"
-  var outputPath = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex1_answer.txt"
-  reverseLines(path, outputPath)
+    var path = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex1.txt"
+    var outputPath = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex1_answer.txt"
+    reverseLines(path, outputPath)
+
+    var path1 = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex2.txt"
+    var outputPath1 = "/Users/christopherspears/Documents/ScalaProjects/impatient_scala_chapter9/src/main/scala/ex2_answer.txt"
+    replaceTabs(path1, outputPath1, 1)
 
 
 def msg = "I was compiled by Scala 3. :)"
 
 
-def reverseLines(path: String, outputPath: String ): Unit =
-  var source = Source.fromFile(path)
-  var lineArray = source.getLines.toArray
-  val out = PrintWriter(outputPath)
-  lineArray.reverse.foreach(out.println)
-  out.close()
+def reverseLines(path: String, outputPath: String): Unit =
+    var source = Source.fromFile(path)
+    var lineArray = source.getLines.toArray
+    val out = PrintWriter(outputPath)
+    lineArray.reverse.foreach(out.println)
+    out.close()
+
+
+def replaceTabs(path: String, outputPath: String, numSpaces: Int): Unit =
+    var source = Source.fromFile(path)
+    val findTabs = """\t+""".r
+    // val spaces = "#" * numSpaces
+    val updatedStr = findTabs.replaceAllIn(source.mkString, "#")
+    println(updatedStr)
+
+    // val out = PrintWriter(outputPath)
+    // out.println(updatedStr)
+    // out.close()
+
 
